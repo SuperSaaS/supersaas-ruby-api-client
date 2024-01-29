460d08c023be773d062d3e56ab114a2eabd7d7fe
Kaarle Kulvik <kaarle@supersaas.com>
2024-01-29 15:05:48 +0100

    Removed a few more carrier \r\n"

--- examples/groups.rb
+++ examples/groups.rb
@@ -2,19 +2,19 @@
 
 require 'supersaas-api-client'
 
-puts "\n\r# SuperSaaS Groups Example\n\r"
+puts "# SuperSaaS Groups Example"
 
 unless Supersaas::Client.instance.account_name && Supersaas::Client.instance.api_key
-  puts "ERROR! Missing account credentials. Rerun the script with your credentials, e.g.\n\r"
-  puts "    SSS_API_ACCOUNT_NAME=<myaccountname> SSS_API_KEY=<xxxxxxxxxxxxxxxxxxxxxx> ./examples/users.rb\n\r"
+  puts "ERROR! Missing account credentials. Rerun the script with your credentials, e.g."
+  puts "SSS_API_ACCOUNT_NAME=<myaccountname> SSS_API_KEY=<xxxxxxxxxxxxxxxxxxxxxx> ./examples/users.rb"
   return
 end
 
 puts "## Account:  #{Supersaas::Client.instance.account_name}"
-puts "## API KEY: #{'*' * Supersaas::Client.instance.api_key.size}\n\r"
+puts "## API KEY: #{'*' * Supersaas::Client.instance.api_key.size}"
 
 Supersaas::Client.instance.verbose = true
 
-puts "\n\rlisting groups..."
-puts "\n\r#### Supersaas::Client.instance.groups.list\n\r"
+puts "listing groups..."
+puts "#### Supersaas::Client.instance.groups.list"
 Supersaas::Client.instance.groups.list
